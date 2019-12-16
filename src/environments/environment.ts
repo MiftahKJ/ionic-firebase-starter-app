d1c8f8b573e2b39fb71f51c238e13c438d140fb4
MiftahKJ <miftahkj28@gmail.com>
2019-12-16 19:41:45 +0700

    masakuy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,14 @@
 export const environment = {
   production: false,
   firebase : {
-    apiKey: "YOUR_CREDENTIALS_HERE",
-    authDomain: "YOUR_CREDENTIALS_HERE",
-    databaseURL: "YOUR_CREDENTIALS_HERE",
-    projectId: "YOUR_CREDENTIALS_HERE",
-    storageBucket: "YOUR_CREDENTIALS_HERE",
-    messagingSenderId: "YOUR_CREDENTIALS_HERE"
+    apiKey: "AIzaSyBrgubCvNFoPHIqUZ3lPsHNeS2NR_re6X0",
+    authDomain: "masakuyv2.firebaseapp.com",
+    databaseURL: "https://masakuyv2.firebaseio.com",
+    projectId: "masakuyv2",
+    storageBucket: "masakuyv2.appspot.com",
+    messagingSenderId: "575801143423",
+    // appId: "1:575801143423:web:785ba662cfbcf1f1534f4a",
+    // measurementId: "G-4X31E4QB86"
   }
 };
 
